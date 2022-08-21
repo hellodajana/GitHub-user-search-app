9fbe6618ce2356a90946f29de7ce788c46edf642
hellodajana <dajanasmolyakov@gmail.com>
2022-08-21 14:43:31 +0200

    Change image and text when color theme changes

--- src/app.js
+++ src/app.js
@@ -2,4 +2,18 @@ const theme = document.getElementById("theme");
 
 theme.addEventListener("click", function () {
   document.body.classList.toggle("dark-theme");
+  changeImageAndText();
 });
+
+function changeImageAndText() {
+  const image = document.getElementById("icon-mode");
+  const text = document.getElementById("theme-name");
+
+  if (image.getAttribute("src") == "assets/icon-moon.svg") {
+    image.src = "assets/icon-sun.svg";
+    text.innerHTML = "Light";
+  } else {
+    image.src = "assets/icon-moon.svg";
+    text.innerHTML = "Dark";
+  }
+}
